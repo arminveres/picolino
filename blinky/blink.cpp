4e573e310263627a3a87e17e7dd0eacac659d9da
arminveres <armin.veres@hotmail.com>
2022-10-20 17:28:42 +0200

    WIP Added stdc++ headers as test for compiling

--- blinky/blink.cpp
+++ blinky/blink.cpp
@@ -1,7 +1,10 @@
 #include <cstdint>
 #include <pico/stdlib.h>
+#include <array>
 
 int main(int argc, char *argv[]) {
+  std::array<int, 10> test;
+  test[0] = 6;
   static constexpr uint8_t LED_PIN = 28;
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
