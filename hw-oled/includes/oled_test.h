bac9b2207ae5f5c472afc59de067b30bb81eff24
Armin Veres <armin.veres@hotmail.com>
2023-01-26 21:10:07 +0100

    Added hello world OLED Example, rewrote given lib structure

---
+++ hw-oled/includes/oled_test.h
@@ -0,0 +1,14 @@
+#ifndef OLED_TEST_H_
+#define OLED_TEST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+int OLED_1in3_C_test(void);
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif  // ! OLED_TEST_H_
---
+++ hw-oled/src/main.cpp
@@ -0,0 +1,6 @@
+#include "oled_test.h"
+
+int main() {
+    OLED_1in3_C_test();
+    return 0;
+}
